8d4b6f5c4bcc1bd46649664a7910df1c6213145b
yamdaddy <46391714+yamdaddy@users.noreply.github.com>
2019-01-05 14:49:30 +0900

    Add files via upload

--- Day5/src/window2/Pizza.java
+++ Day5/src/window2/Pizza.java
@@ -7,8 +7,11 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Pizza {
 
@@ -22,31 +25,51 @@ public class Pizza {
 	int bul = 0;
 
 	JTextArea list;
+	private JLabel label;
+	private JButton btnNewButton;
 
 	public Pizza() {
 
 		f = new JFrame();
-		f.setSize(350, 200);
+		f.setSize(354, 223);
 
 		top = new JLabel("자바 피자에 오신것을 환영합니다. 피자를 선택해 주세요.");
 		count = new JLabel("개수");
-		b1 = new JButton("콤보피자");
+		b1 = new JButton("\uCF64\uBCF4\uD53C\uC790!!!!");
+		b1.setFont(new Font("HY엽서L", Font.PLAIN, 12));
 		b2 = new JButton("포테이토피자");
+		b2.setFont(new Font("HY엽서L", Font.PLAIN, 12));
 		b3 = new JButton("불고기피자");
+		b3.setFont(new Font("HY엽서L", Font.PLAIN, 12));
 		total = new JTextField(20);
 
 		FlowLayout flow = new FlowLayout();
-		f.setLayout(flow);
+		f.getContentPane().setLayout(flow);
 
 		list = new JTextArea(3, 30);
 
-		f.add(top);
-		f.add(b1);
-		f.add(b2);
-		f.add(b3);
-		f.add(count);
-		f.add(total);
-		f.add(list);
+		f.getContentPane().add(top);
+		f.getContentPane().add(b1);
+		f.getContentPane().add(b2);
+		f.getContentPane().add(b3);
+		f.getContentPane().add(count);
+		f.getContentPane().add(total);
+		f.getContentPane().add(list);
+		
+		label = new JLabel("\uCD1D\uACC4");
+		label.setFont(new Font("HY궁서B", Font.PLAIN, 18));
+		label.setBackground(Color.YELLOW);
+		f.getContentPane().add(label);
+		
+		btnNewButton = new JButton("\uB098\uB97C \uB20C\uB7EC\uC694!!");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "나를 정말 누르셨군요");
+			}
+		});
+		f.getContentPane().add(btnNewButton);
 
 		b1.addActionListener(new ActionListener() {
 
