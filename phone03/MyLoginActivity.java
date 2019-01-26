efb8cd1fd1a5e8b9cbae5b13ec05939456020dfe
yamdaddy <46391714+yamdaddy@users.noreply.github.com>
2019-01-26 14:46:02 +0900

    Add files via upload

--- phone03/MyLoginActivity.java
+++ phone03/MyLoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MyLoginActivity extends AppCompatActivity {
@@ -22,8 +23,25 @@ public class MyLoginActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MemoActivity.class);
                 startActivity(intent);
             }
+
+
         });
 
+        EditText idText = findViewById(R.id.idText);
+        EditText pwText = findViewById(R.id.pwText);
+
+        String id = idText.getText().toString();
+        String pw = pwText.getText().toString();
+
+        String oriId = "admin";
+        String oriPw = "pass";
+
+        if (id.equals(oriId) && pw.equals(oriPw)){
+            Toast.makeText(getApplicationContext(), "로그인 성공, 메모장으로 이동합니다.", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getApplicationContext(), "로그인 실패, 재 로그인 해주세요.", Toast.LENGTH_SHORT).show();
+
+        }
 
 
 
